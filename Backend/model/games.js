f61a2eadfe5b3aa0b5a4bbee4a2c29a80e23f87c
Simamkele <princetancu06@gmail.com>
2024-10-10 05:42:06 +0200

    Update

--- Backend/model/games.js
+++ Backend/model/games.js
@@ -15,7 +15,7 @@ class Games {
     this.myHeaders.append("Authorization", `Bearer ${token}`); 
   }
 
-  // Method to fetch games
+  //  fetch games
   async fetchGames(req, res) {
     const raw =
       "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;";
@@ -43,7 +43,7 @@ class Games {
     }
   }
 
-  // Method to fetch a single game by ID
+//  fetch a single game by ID
   async fetchGame(req, res) {
     const gameId = req.params.id; 
     const raw = `fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites; where id = ${gameId};`;
