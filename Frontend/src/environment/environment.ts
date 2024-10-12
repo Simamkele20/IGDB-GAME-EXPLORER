7942f67f1a32e65028486a7827be59924652c701
Simamkele <princetancu06@gmail.com>
2024-10-12 07:39:08 +0200

    Updates

--- Frontend/src/environment/environment.ts
+++ Frontend/src/environment/environment.ts
@@ -3,8 +3,7 @@ export const environment = {
     production: false,
     apiUrl: 'https://igdb-game-explorer.onrender.com',
     clientId: '0oahrr9tqq4ez9uc2170n9pgvhk8v1',
-    Games_URL: 'https://igdb-game-explorer.onrender.com/games',
-    GAME_BY_ID_URL: 'https://igdb-game-explorer.onrender.com/games/',
-    Search_URL: 'https://igdb-game-explorer.onrender.com/search/'
+    MultiQuery_URL: 'https://api.igdb.com/v4/multiquery',
+
   };
   
